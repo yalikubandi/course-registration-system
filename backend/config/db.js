342b92a9428ca076fd1c45bfab6aff9e3085b27a
Yalikubandika <mengiyalikubandi34@gmail.com>
2025-05-29 15:57:45 +0300

    More DB updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,28 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root', // Default XAMPP MySQL user
+//     password: '', // Default XAMPP MySQL password (empty)
+//     database: 'platform_db'
+// });
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Default XAMPP MySQL user
-    password: '', // Default XAMPP MySQL password (empty)
-    database: 'platform_db'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: process.env.NODE_ENV === 'production'? { rejectUnauthorized: false } : false
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err.stack);
-        return;
-    }
-    console.log('Connected to MySQL as id', connection.threadId);
+connection.connect(err => {
+  if (err) {
+    return console.error('Database connection failed:', err.stack);
+  }
+  console.log('Connected to MySQL as ID', connection.threadId);
 });
+
 
 module.exports = connection;
